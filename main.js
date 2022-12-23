df3a89fda2c5b3414c5339208080f1680b880d2e
Moritz (Tower) <Moritz.Kuhn@gmail.com>
2022-12-24 00:49:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ class EcoflowMqtt extends utils.Adapter {
 		if (state) {
 			// The state was changed
 			//this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-			if (${id} == "mqtt.0.app.device.property.DAEBZ5ZD9180661") {
+			if (${id} = "mqtt.0.app.device.property.DAEBZ5ZD9180661") {
 				const state2 = getState("mqtt.0.app.device.property.DAEBZ5ZD9180661").val;
 				this.log.info(`state ${id} was updated`);
 			}
