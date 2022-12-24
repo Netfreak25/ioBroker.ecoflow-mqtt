53b664da0babc5b6c353e3d2f2b57520cb50f00b
Moritz (Tower) <Moritz.Kuhn@gmail.com>
2022-12-24 01:25:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -169,7 +169,9 @@ class EcoflowMqtt extends utils.Adapter {
 				var json = JSON.parse(state.val)
 				var string = JSON.stringify(json,'$.params')
 				var params = this.subsequenceFromStartFromEnd(string, 1, 1)
-				var list = "" + params.split(',');
+
+				this.log.info(list);
+				//var list = "" + params.split(',');
 
 			}
 		}
